c4cd8031a97633e1fef18c509e7b0884c1103738
Salazar-afk <83561083+Salazar-afk@users.noreply.github.com>
2022-10-28 20:18:50 -0300

    carga de carpeta de prueba
    una carpeta de prueba

---
+++ pong en python/solucion1.py
@@ -0,0 +1,16 @@
+import pygame 
+import sys
+from pygame.locals import *
+
+pygame.init()
+
+pantalla = pygame.display.set_mode((500, 400))
+
+pygame.display.set_caption('Mi primer juego :D')
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+   
